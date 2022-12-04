092bf3cf586b9b7f9055f62c1cdedad4ad7853ab
Nikolas Papadopoulos <53314070+NikolasPpd@users.noreply.github.com>
2022-12-04 21:25:25 +0200

    move settings to different file

--- docs/app.js
+++ docs/app.js
@@ -154,72 +154,6 @@ function calculateColorHue(efficiency) {
     return hue;
 }
 
-// ***************************************************
-//                    SETTINGS
-// ***************************************************
-//all available settings
-let allSettings = {
-    //distance
-    m: {
-        unit: 'm',
-        power: 0
-    },
-    km: {
-        unit: 'km',
-        power: 3
-    },
-    //velocity
-    ms: {
-        unit: 'm/s',
-        power: 0
-    },
-    kms: {
-        unit: 'km/s',
-        power: 3
-    },
-    //packet size
-    bits: {
-        unit: 'bits',
-        power: 0
-    },
-    kbits: {
-        unit: 'Kb',
-        power: 3
-    },
-    mbits: {
-        unit: 'Mb',
-        power: 6
-    },
-    gbits: {
-        unit: 'Gb',
-        power: 9
-    },
-    tbits: {
-        unit: 'Tb',
-        power: 12
-    },
-    //bitrate
-    bps: {
-        unit: 'bits/s',
-        power: 0
-    },
-    kbps: {
-        unit: 'Kb/s',
-        power: 3
-    },
-    mbps: {
-        unit: 'Mb/s',
-        power: 6
-    },
-    gbps: {
-        unit: 'Gb/s',
-        power: 9
-    },
-    tbps: {
-        unit: 'Tb/s',
-        power: 12
-    }
-}
 //start with default settings
 let settings = {
     distance: {
@@ -283,7 +217,7 @@ function loadSettings() {
 // temporarily save settings in tempSettings
 function updateTempSettings(button) {
     let dataOption = button.parentNode.parentNode.getAttribute('data-option');
-    
+
     tempSettings[dataOption].unit = allSettings[button.value].unit;
     tempSettings[dataOption].power = allSettings[button.value].power;
     tempSettings[dataOption].selectionIndex = Array.from(button.parentNode.children).indexOf(button);
